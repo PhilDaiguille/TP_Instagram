496e25d1d6bbe94e4f9f3d0dba8eaee54c6abd4e
PhilDaiguille <philippe.delente@gmail.com>
2021-12-18 22:13:58 +0100

    update Css/JS

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("chargé");
     let btn, warning, email, password;
-    expr = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    let exprmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    let exprmdp = /^[a-zA-Z0-9._-]{2,15}$/;
 
     warning = document.querySelector(".form p");
     email = document.getElementsByTagName("input")[0].value;
     password = document.getElementsByTagName("input")[1].value;
     btn = document.getElementsByTagName("input")[2];
-    /* evenement */
+
     btn.addEventListener("click", e => {
         e.preventDefault();
         formActive();
@@ -16,20 +17,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
         email = document.getElementsByTagName("input")[0].value;
         password = document.getElementsByTagName("input")[1].value;
-        submit = document.getElementsByTagName("input")[2].value;
 
-        if (email && '@' && password) {
+        if (email.match(exprmail) && password.match(exprmdp)) {
             warning.classList.add("success");
             warning.classList.remove("error");
-            warning.innerText = "vous etes logué";
-            localStorage.setItem("user", email); //stocker le login
+            warning.innerText = "Connexion réussie";
+            localStorage.setItem("user", email);
             sessionStorage.setItem("user", "sessionid");
             document.location.assign("accueil.html");
-        } else if (password) {
+        } else if (password.match(exprmdp)) {
             warning.classList.add("error");
             warning.innerText = "Mail invalide";
             warning.classList.remove("success");
-        } else if (email) {
+        } else if (email.match(exprmail)) {
             warning.classList.add("error");
             warning.innerText = "Mot de passe invalide";
             warning.classList.remove("success");
--- js/appAccueil.js
+++ js/appAccueil.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("chargé");
     let submit_log = localStorage.getItem("user");
-    let submit_session = sessionStorage.getItem("session");
     let el = document.getElementsByTagName("h2")[0];
     let el2 = document.getElementsByTagName("h2")[1];
     el.innerText += ` ${submit_log}`;
